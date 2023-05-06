0ae2dca613b0cb3c7be6d86ba940ade88e444af1
Ivanovich0705 <ivantenis1@icloud.com>
2023-05-05 19:11:58 -0500

    added report

--- ClientService/src/main/java/com/fastporte/ClientService/service/IClientService.java
+++
@@ -1,8 +0,0 @@
-package com.fastporte.ClientService.service;
-
-import com.fastporte.ClientService.entities.Client;
-
-public interface IClientService extends CrudService<Client> {
-
-    Client findByEmailAndPassword(String email, String password) throws Exception;
-}
---
+++ src/main/java/com/fastporte/reportmicroservice/ReportMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.fastporte.reportmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ReportMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportMicroserviceApplication.class, args);
+    }
+
+}
